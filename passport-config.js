3331bcadf4c50fbc4143d6c698ff103ff0d07805
Jajojo_v <126989644+Jajojovp@users.noreply.github.com>
2023-04-12 11:44:59 -0400

    passport-config.js
    para modificar e implementar la autenticación y manejo de sesiones (Passport.js)

---
+++ passport-config.js
@@ -0,0 +1,26 @@
+// passport-config.js
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+module.exports = function(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (!user) {
+      return done(null, false, { message: 'No existe un usuario con ese correo electrónico' });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Contraseña incorrecta' });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id
