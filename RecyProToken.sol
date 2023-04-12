9f15264d3afd7d8c1cef29f3b67a335cd6f99323
Jajojo_v <126989644+Jajojovp@users.noreply.github.com>
2023-04-12 11:47:21 -0400

    RecyProToken.sol
    Para la creación y manejo de tokens (Ethereum, Solidity y Web3.js)

---
+++ RecyProToken.sol
@@ -0,0 +1,10 @@
+// RecyProToken.sol
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RecyProToken is ERC20 {
+    constructor() ERC20("RecyProToken", "RPT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
